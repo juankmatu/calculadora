00bd12271343346ee9e4d4be3d6c637496d0b74e
juankmatu <44886702+juankmatu@users.noreply.github.com>
2020-12-19 13:55:15 -0300

    calculadora
    sur guard

---
+++ calculador de extras.py
@@ -0,0 +1,15 @@
+mañana=154
+Nocturnas=30.80
+
+Horas_dia=int(input("horas de mañana: "))
+
+total1=mañana*Horas_dia
+
+Horas_noctu=int(input("horas de noches: "))
+
+total2=Nocturnas*Horas_noctu
+
+Efectivo_total=int(total1 + total2)
+#print(total2)
+#print(total1)
+print(Efectivo_total) 
